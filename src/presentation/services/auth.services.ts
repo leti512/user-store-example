7fadc69a389c5199bd3125fb310b5041f5309e6b
Leticia Diaz <leticiadiaz512@gmail.com>
2024-10-20 22:43:38 -0600

    :hammer: FEAT: Create user

--- src/presentation/services/auth.services.ts
+++ src/presentation/services/auth.services.ts
@@ -1,5 +1,5 @@
 
-import { CustomError, RegisterUserDto } from "../../domain";
+import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
 import { UserModel } from "../../data";
 
 export class AuthService{
@@ -10,10 +10,26 @@ export class AuthService{
     public async registerUser( registerUserDto: RegisterUserDto ){
 
         const existUser = await UserModel.findOne({ email: registerUserDto.email });
-        console.log(existUser)
+        
         if ( existUser ) throw CustomError.badRequest('Email already exist');
 
-        return 'todo ok!'
+        try {
+
+            const user = new UserModel(registerUserDto);
+            await user.save();
+
+            const {password, ...rest} = UserEntity.fromObject(user);
+
+            //const userEntity = UserEntity.fromObject(user);
+
+            return {
+                user: rest,
+                token: 'ABC'
+            };
+
+        } catch (error) {
+            throw CustomError.internalServer(`${ error }`);
+        }
 
     }
 }
